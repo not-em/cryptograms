476391bfe60de1cee9de8d994043b476b0f038f8
not-em <emily.williams764@gmail.com>
2026-03-15 17:43:55 +0000

    feat: add solver module and update package exports

---
+++ cryptograms/__init__.py
@@ -0,0 +1,11 @@
+"""Cryptogram solver package."""
+
+from __future__ import annotations
+
+from .service import solve_cryptogram, solve_file
+
+__all__ = [
+    "solve_cryptogram",
+    "solve_file",
+]
+
--- cryptograms/core/__init__.py
+++ cryptograms/core/__init__.py
@@ -2,10 +2,12 @@
 
 from .exceptions import CryptogramError
 from .models import CipherMapping, Puzzle, Solution
+from .solver import Solver
 
 __all__ = [
     "Puzzle",
     "Solution",
     "CipherMapping",
     "CryptogramError",
+    "Solver",
 ]
